19a3e65a2ce1ca73356ca98371fb838ce56419cc
soyeb <sopher.yeb.1@ulaval.ca>
2014-12-05 11:08:05 -0500

    Add sound: working

--- web/racer_main.dart
+++ web/racer_main.dart
@@ -4,12 +4,13 @@ import 'dart:html';
 import 'dart:async';
 import 'dart:math';
 import 'package:box2d/box2d.dart'; // Package used in controlling physics
-
+import 'package:stagexl/stagexl.dart';
 
 
 part 'board.dart';
 part 'race_car.dart';
 part 'background.dart';
+//part 'sound.dart';
 
 
 
@@ -17,4 +18,12 @@ void main() {
   //get a reference to the canvas
   CanvasElement canvas = document.querySelector('#canvas');
   Board board = new Board(canvas);
+
+  var resourceManager = new ResourceManager();
+  resourceManager.addSound("map", "media/map1.mp3");
+  resourceManager.load().then((_) {
+  resourceManager.getSound("map").play();
+});
+
 }
+
