1553d31fe4914ce7ac41c3966dcf27e12fbdfacc
williamgiguere <info@williamgiguere.com>
2014-12-14 19:03:19 -0500

    added map2 and map class

---
+++ web/RaceMapClass.dart
@@ -0,0 +1,12 @@
+part of super_racer;
+
+class RaceMap {
+  
+  ImageElement background;
+  List collisions;
+  num startx;
+  num starty;
+  
+  RaceMap(this.background, this.collisions, this.startx, this.starty);
+  
+}
--- web/board.dart
+++ web/board.dart
@@ -1,10 +1,8 @@
 part of super_racer;
 
 class Board {
-
-  //Player starting position
-  static const num START_X = 447;
-  static const num START_Y = 290;
+  
+  RaceMap raceMap;
 
   //Using time to redraw board 
   Timer timer;
@@ -12,22 +10,11 @@ class Board {
   CanvasElement canvas;
   CanvasRenderingContext2D context;
   
-  
   //Board size (using width and height to fit canvas size)
   num width;
   num height;
   
   RaceCar racecar;
-  
-  //List of collision points
-  List collisions = [{'x': 84, 'y': 80, 'width': 52, 'height': 216},
-                     {'x': 68, 'y': 276, 'width': 20, 'height': 56},
-                     {'x': 136, 'y': 188, 'width': 208, 'height': 60},
-                     {'x': 344, 'y': 208, 'width': 64, 'height': 40},
-                     {'x': 368, 'y': 248, 'width': 40, 'height': 160},
-                     {'x': 368, 'y': 4, 'width': 140, 'height': 76},
-                     {'x': 4, 'y': 436, 'width': 236, 'height': 72}
-                     ];
  
   //Boolean to indicate car collision
   bool canMoveLeft = true;
@@ -36,20 +23,22 @@ class Board {
   bool canMoveBack = true;
   
   //Drawing the board to fit the canvas
-  Board(this.canvas) {
+  Board(this.canvas, this.raceMap) {
     context = canvas.getContext("2d");
     width = canvas.width;
     height = canvas.height;   
     
     //Using the start game button to start game
-    querySelector('#play1').onClick.listen((e) {
-      init();
-    });
+    init();
   }
   
   //Initial 'Draw'
   void init() {
-    racecar = new RaceCar(this, START_X, START_Y);
+    context.beginPath();
+    context.drawImage(raceMap.background, 0, 0);
+    context.closePath();
+    context.fill();
+    racecar = new RaceCar(this, raceMap.startx, raceMap.starty);
     // redraw every 10 ms
     timer = new Timer.periodic(const Duration(milliseconds: 10),
         // 't' to call back timer to redraw board every duration
@@ -64,15 +53,19 @@ class Board {
   void redraw() {
     clear();
     racecar.draw();
+    context.beginPath();
+    context.drawImage(raceMap.background, 0, 0);
+    context.closePath();
+    context.fill();
  
     
 //Right collisions
     
-    for (var i = 0; i < collisions.length; i++) {
-      if (collisions.elementAt(i)['x'] <= racecar.carPositionX + racecar.carWidth &&
-       collisions.elementAt(i)['x'] + collisions.elementAt(i)['width'] > racecar.carPositionX &&
-       collisions.elementAt(i)['y'] < racecar.carPositionY + racecar.carHeight &&
-       collisions.elementAt(i)['height'] + collisions.elementAt(i)['y'] > racecar.carPositionY ||
+    for (var i = 0; i < raceMap.collisions.length; i++) {
+      if (raceMap.collisions.elementAt(i)['x'] <= racecar.carPositionX + racecar.carWidth &&
+       raceMap.collisions.elementAt(i)['x'] + raceMap.collisions.elementAt(i)['width'] > racecar.carPositionX &&
+       raceMap.collisions.elementAt(i)['y'] < racecar.carPositionY + racecar.carHeight &&
+       raceMap.collisions.elementAt(i)['height'] + raceMap.collisions.elementAt(i)['y'] > racecar.carPositionY ||
        racecar.carPositionX >= 498) {
          canMoveRight = false; {racecar.carPositionX -= racecar.collisionImpact; 
        }
@@ -83,11 +76,11 @@ class Board {
     }
 
     //Left collisions
-    for (var i = 0; i < collisions.length; i++) {
-      if (collisions.elementAt(i)['x'] < racecar.carPositionX + racecar.carWidth &&
-      collisions.elementAt(i)['x'] + collisions.elementAt(i)['width'] >= racecar.carPositionX &&
-      collisions.elementAt(i)['y'] < racecar.carPositionY + racecar.carHeight &&
-      collisions.elementAt(i)['height'] + collisions.elementAt(i)['y'] > racecar.carPositionY ||
+    for (var i = 0; i < raceMap.collisions.length; i++) {
+      if (raceMap.collisions.elementAt(i)['x'] < racecar.carPositionX + racecar.carWidth &&
+      raceMap.collisions.elementAt(i)['x'] + raceMap.collisions.elementAt(i)['width'] >= racecar.carPositionX &&
+      raceMap.collisions.elementAt(i)['y'] < racecar.carPositionY + racecar.carHeight &&
+      raceMap.collisions.elementAt(i)['height'] + raceMap.collisions.elementAt(i)['y'] > racecar.carPositionY ||
       racecar.carPositionX <= 4) {
         canMoveLeft = false; {racecar.carPositionX += racecar.collisionImpact; 
         }
@@ -99,11 +92,11 @@ class Board {
           
           
     //Front collisions
-    for (var i = 0; i < collisions.length; i++) {
-      if (collisions.elementAt(i)['x'] < racecar.carPositionX + racecar.carWidth &&
-      collisions.elementAt(i)['x'] + collisions.elementAt(i)['width'] > racecar.carPositionX &&
-      collisions.elementAt(i)['y'] < racecar.carPositionY + racecar.carHeight &&
-      collisions.elementAt(i)['height'] + collisions.elementAt(i)['y'] >= racecar.carPositionY ||
+    for (var i = 0; i < raceMap.collisions.length; i++) {
+      if (raceMap.collisions.elementAt(i)['x'] < racecar.carPositionX + racecar.carWidth &&
+      raceMap.collisions.elementAt(i)['x'] + raceMap.collisions.elementAt(i)['width'] > racecar.carPositionX &&
+      raceMap.collisions.elementAt(i)['y'] < racecar.carPositionY + racecar.carHeight &&
+      raceMap.collisions.elementAt(i)['height'] + raceMap.collisions.elementAt(i)['y'] >= racecar.carPositionY ||
       racecar.carPositionY <= 4) {
         canMoveFront = false; {racecar.carPositionY += racecar.collisionImpact; 
         }
@@ -114,11 +107,11 @@ class Board {
     }
              
     //Back collisions
-    for (var i = 0; i < collisions.length; i++) {
-       if (collisions.elementAt(i)['x'] < racecar.carPositionX + racecar.carWidth &&
-       collisions.elementAt(i)['x'] + collisions.elementAt(i)['width'] > racecar.carPositionX &&
-       collisions.elementAt(i)['y'] <= racecar.carPositionY + racecar.carHeight &&
-       collisions.elementAt(i)['height'] + collisions.elementAt(i)['y'] > racecar.carPositionY ||
+    for (var i = 0; i < raceMap.collisions.length; i++) {
+       if (raceMap.collisions.elementAt(i)['x'] < racecar.carPositionX + racecar.carWidth &&
+       raceMap.collisions.elementAt(i)['x'] + raceMap.collisions.elementAt(i)['width'] > racecar.carPositionX &&
+       raceMap.collisions.elementAt(i)['y'] <= racecar.carPositionY + racecar.carHeight &&
+       raceMap.collisions.elementAt(i)['height'] + raceMap.collisions.elementAt(i)['y'] > racecar.carPositionY ||
        racecar.carPositionY >= 498) {
          canMoveBack = false; {racecar.carPositionY -= racecar.collisionImpact;
          }
@@ -128,16 +121,16 @@ class Board {
        }  
     }
                              
-       
-    // Drawing the car to it's new position on the board 
-    if (racecar.backDown && canMoveBack) {racecar.carPositionY += racecar.speed;
-    }
-    if (racecar.frontDown && canMoveFront) {racecar.carPositionY -= racecar.speed; 
-    }
-    if (racecar.rightDown && canMoveRight) { racecar.carPositionX += racecar.speed;
-    }
-    if (racecar.leftDown && canMoveLeft) {racecar.carPositionX -= racecar.speed;
-    }
+
+    //Diagonal speed adjustments
+   if (racecar.backDown && canMoveBack) {racecar.carPositionY += racecar.speed;
+   }
+   if (racecar.frontDown && canMoveFront) {racecar.carPositionY -= racecar.speed;
+   }
+   if (racecar.rightDown && canMoveRight) { racecar.carPositionX += racecar.speed;
+   }
+   if (racecar.leftDown && canMoveLeft) {racecar.carPositionX -= racecar.speed;
+   }
     racecar.draw();
   }
 }
